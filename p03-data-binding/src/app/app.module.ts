15f15111aeb2982b7515a565746ee2a9d30eef56
Wedson Quintanilha da Silva <Wedson Quintanilha da Silva>
2017-04-15 14:48:33 -0300

    aula modulos

--- p03-data-binding/src/app/app.module.ts
+++ p03-data-binding/src/app/app.module.ts
@@ -6,11 +6,17 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
+  //Componentes, diretivas e pipes
   declarations: [AppComponent, DataBindingComponent, MeuPrimeiro2Component],
-  imports: [BrowserModule,FormsModule,HttpModule],
+  //Outros módulos que serão utilizados nesse módulo ou em algum componente utilizado a esse módulo
+  imports: [BrowserModule,FormsModule,HttpModule, CursosModule],
+  //Escopo global -> Serviços que ficarão disponíveis para todos os componentes declarados nesse módulo (escopo para a aplicação)
   providers: [],
+  //Apenas no módulo raiz da app - qual é o componente que será o container da aplicação
+  //Aqui que serão configuradas as rotas, onde o menu da applicação será montado e chamado
   bootstrap: [AppComponent]
 })
 export class AppModule { }
